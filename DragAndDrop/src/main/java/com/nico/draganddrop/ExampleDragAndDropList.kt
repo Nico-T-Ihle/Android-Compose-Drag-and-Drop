9591368eaaf64262dded0f24ee5c30648a70da75
Nico Ihle <90788099+Nico-T-Ihle@users.noreply.github.com>
2026-06-28 00:33:42 +0200

    Update ExampleDragAndDropList.kt

--- DragAndDrop/src/main/java/com/nico/draganddrop/ExampleDragAndDropList.kt
+++ DragAndDrop/src/main/java/com/nico/draganddrop/ExampleDragAndDropList.kt
@@ -1,23 +1,23 @@
 package com.nico.draganddrop
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,18 +29,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 
-@SuppressLint("MutableCollectionMutableState")
 @Composable
-fun ExampleDragAndDropList(){
+fun ExampleDragAndDropList() {
 
-    /** Replace @useYourList with your list! but note it has to be a mutableStateOf! **/
-    var useYourList by remember { mutableStateOf(mutableListOf(1, 2, 3)) }
+    val useYourList = remember { mutableStateListOf(1, 2, 3) }
 
     fun dragAndDropUpdateList(fromId: Int, toId: Int) {
-
         val fromIndex = useYourList.indexOf(fromId)
         val toIndex = useYourList.indexOf(toId)
-
         if (fromIndex != -1 && toIndex != -1) {
             val temp = useYourList[fromIndex]
             useYourList[fromIndex] = useYourList[toIndex]
@@ -48,8 +44,11 @@ fun ExampleDragAndDropList(){
         }
     }
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.Blue)
+    ) {
         LazyVerticalGrid(
+            modifier = Modifier.fillMaxHeight(),
             columns = GridCells.Fixed(3),
             horizontalArrangement = Arrangement.spacedBy(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -77,36 +76,33 @@ fun ExampleDragAndDropList(){
                             getDataType = 0,
                             item = item,
                             modifier = Modifier,
-                        ) { isInBound, data ->
+                        ) { isInBound ->
 
                             if (isInBound) {
                                 hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
                             }
 
-                            data?.let {
-                                dragAndDropUpdateList(item, data)
-                            }
-
                             DragTarget(
                                 isActive = true,
                                 modifier = Modifier,
-                                dataToDrop = item
+                                dataToDrop = item,
+                                onSwap = { from, to ->
+                                    dragAndDropUpdateList(from, to)
+                                }
                             ) {
                                 Column(
                                     horizontalAlignment = Alignment.CenterHorizontally,
                                     verticalArrangement = Arrangement.Center,
                                     modifier = Modifier
                                         .padding(8.dp)
+                                        .height(100.dp)
+                                        .width(100.dp)
                                         .fillMaxWidth()
                                         .background(
-                                            if (isInBound) {
-                                                Color.Green
-                                            } else {
-                                                Color.Red
-                                            }
+                                            if (isInBound) Color.Green else Color.Red
                                         )
                                 ) {
-                                    Text(text = "$item", Modifier.padding(), fontSize = 16.sp)
+                                    Text(text = "$item", fontSize = 16.sp)
                                 }
                             }
                         }
